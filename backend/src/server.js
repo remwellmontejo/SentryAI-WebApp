00d7a1fa21fb520205c945c20aeafd21bab4e2d5
remwellmontejo <remwellmontejo@gmail.com>
2025-12-04 15:48:31 +0800

    for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 import apprehendedCarRoutes from './routes/ApprehendedVehicleRoutes.js';
 import connectDB from './config/db.js';
 import authRoutes from './routes/AuthRoutes.js';
 
 const app = express();
+const __dirname = path.resolve();
 dotenv.config();
-connectDB();
 
-app.use(cors());
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: 'http://localhost:5173',
+    }));
+}
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(
@@ -25,7 +31,17 @@ app.use(
 app.use("/api/apprehended-vehicle", apprehendedCarRoutes);
 app.use("/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+connectDB().then(() => {
+    app.listen(5001, () => {
+        console.log('Server is running on port 5001');
+    });
 });
 
