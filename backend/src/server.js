13be377c048cf1f7c7ae04393299aa1607142538
remwellmontejo <remwellmontejo@gmail.com>
2025-12-08 17:23:35 +0800

    changed base url

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,6 +37,8 @@ if (process.env.NODE_ENV === 'production') {
     app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
+
+
 }
 
 connectDB().then(() => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/" : "";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/" : "https://sentryai.onrender.com";
 
 const api = axios.create({
     baseURL: BASE_URL,
