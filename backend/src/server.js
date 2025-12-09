526efc2780757ecd7f32470a7d57047a4c14b6bb
remwellmontejo <remwellmontejo@gmail.com>
2025-12-09 14:16:36 +0800

    server config

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,11 +18,11 @@ if (process.env.NODE_ENV !== 'production') {
     }));
 }
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(
     bodyParser.urlencoded({
-        limit: "50mb",
+        limit: "100mb",
         extended: true,
         parameterLimit: 300000,
     }),
