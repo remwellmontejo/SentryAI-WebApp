392f530060feaa7913d75df383d1b89c3fe0391a
remwellmontejo <remwellmontejo@gmail.com>
2025-12-12 13:31:38 +0800

    adjust centroid

--- frontend/src/pages/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage.jsx
@@ -5,7 +5,7 @@ import api from "../lib/axios.js";
 import Navbar from "../components/Navbar";
 
 const getSquarePosition = (x, y, modelSize) => {
-    const percentX = ((x + 12) / modelSize) * 100;
+    const percentX = ((x) / modelSize) * 100;
     const percentY = ((y) / modelSize) * 100;
     return {
         // Clamp between 0-100% to keep marker inside the box
